d739819e175381b6695540fe2b486a80143f82aa
Gus657 <ggusperezg@gmail.com>
2020-03-30 03:47:50 -0600

    Login Funcional V1

---
+++ seguridad-rup/seguridad-rup/Querys/SQL-Login.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace seguridad_rup
+{
+	
+	class SQL_Login
+	{
+		SQL_Conexion conectar = new SQL_Conexion();
+		public string Ingresar(string User, string Pass, string modulo) {
+			bool  succes = ConsultarUsuario(User, Pass);
+			string respuesta = "";
+			if (succes)
+			{
+				string sql = "SELECT permiso FROM permisos_modulos WHERE id_usuario= " +
+				"(Select id_usuario from usuarios where nombre_usuario ='" + User + "') AND id_modulo=" + modulo + " AND estatus ='activo';";
+				OdbcCommand command = new OdbcCommand(sql, conectar.conexion());
+				OdbcDataReader reader = command.ExecuteReader();
+				if (reader.HasRows)
+				{
+					respuesta = "Exito";
+				}
+				else
+				{
+					respuesta = "Fallo";
+				}
+			}
+			else { respuesta = "No Existe"; }
+			return respuesta;
+		}
+		bool ConsultarUsuario(string User, string Pass) {
+			string sql = "SELECT estatus FROM usuarios WHERE nombre_usuario='" + User + "' AND clave=MD5('" + Pass + "');";
+			bool respuesta = false;
+			OdbcCommand command = new OdbcCommand(sql, conectar.conexion());
+			OdbcDataReader reader = command.ExecuteReader();
+			if (reader.HasRows && reader.GetValue(0).ToString() == "activo")
+			{
+				respuesta = true;
+			}
+			else
+			{
+				respuesta = false;
+			}
+			return respuesta;
+		}
+
+	}
+}
---
+++ seguridad-rup/seguridad-rup/Vistas/Login.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace seguridad_rup.Vistas
+{
+	public partial class Frm_Login : Form
+	{
+		string modulo = "";
+		string mensajeFallo = "";
+		string mensajePermiso = "";
+		public Frm_Login(string mod)
+		{
+			modulo = mod;
+			InitializeComponent();
+		}
+
+		void ESP_Selecionado() {
+			Lbl_Usuario.Text = "Usuario";
+			Lbl_Clave.Text = "Contraseña";
+			Btn_ingresar.Text = "Entrar";
+			Btn_limpiar.Text = "Limpiar";
+			Lnk_olvideClave.Text = "Olvidé mí contrseña...";
+			mensajeFallo = "El usuario o contraseña no existe.";
+			mensajePermiso = "No posee permisos sobre este Modulo.";
+		}
+
+		void ENG_Selecionado()
+		{
+			Lbl_Usuario.Text = "User";
+			Lbl_Clave.Text = "Pass";
+			Btn_ingresar.Text = "Accept";
+			Btn_limpiar.Text = "Clean";
+			Lnk_olvideClave.Text = "Maybe I Forgot my Pass...";
+			mensajeFallo = "The User or pass doesn't exist";
+			mensajePermiso = "You don't have permissions on this module.";
+		}
+		private void Panel1_Paint(object sender, PaintEventArgs e)
+		{
+
+		}
+
+		private void Label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void ComboBox1_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			e.Handled = true;
+		}
+
+		private void Cmb_Lenguajes_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			switch (Cmb_Lenguajes.Text.ToString())
+			{
+				case "ESP":
+					ESP_Selecionado();
+					break;
+				case "ENG":
+					ENG_Selecionado();
+					break;
+				default:
+					break;
+			}
+			
+		}
+
+		private void Frm_Login_Load(object sender, EventArgs e)
+		{
+			ESP_Selecionado();
+		}
+
+		private void Btn_ingresar_Click(object sender, EventArgs e)
+		{
+			SQL_Login loginSQL = new SQL_Login();
+			String succes = loginSQL.Ingresar(Txt_Usuarios.Text.ToString(), Txt_Clave.Text.ToString(), modulo);
+			if (succes == "Exito")
+			{
+				this.Visible = false;
+			}
+			else if (succes == "Fallo")
+			{
+				MessageBox.Show(mensajePermiso);
+			}
+			else {
+				MessageBox.Show(mensajeFallo);
+			}
+			
+		}
+
+		private void Txt_Clave_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
